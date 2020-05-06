0177db1b75900526f0dd05e125c7587e7315bbbd
Joe Shields <shields.john.joseph@gmail.com>
2020-05-06 16:18:44 -0500

    updated bio

--- reactportfolio/src/Components/About/About.jsx
+++ reactportfolio/src/Components/About/About.jsx
@@ -25,17 +25,22 @@ function About() {
         <div className="AboutBox col-md-8 shadow-lg center rounded mt-5">
           <p className="card-text text-white font-weight-bold">
             Software Engineer skilled in both back-end and front-end, with
-            particular interests in design and UX-UI experience. My career as a
-            director of market research sparked my interests and brought me
-            closer to my true passion of creating web applications. I like to be
-            creative and unique, but I also enjoy the technical challenges of
-            back-end development. When programming I follow structure and order,
-            since they are essential when creating quality web applications. I
-            enjoy spending time fixing small details, and optimizing
-            applications. Working in a team setting is ideal, since more
-            knowledge will be obtained between developers. In the next phase of
-            my career, I want to continue to be challenged and tested, as a
-            means to maintain growth as a developer.
+            particular interest in design and UX-UI experience. My career as a
+            director of market research inspired me to pursue my true passion of
+            creating web applications. I love being creative and unique, but I
+            also appreciate the technical challenges of back-end development.
+            When programming I follow structure and order, since this is
+            essential when creating quality web applications. I enjoy debugging
+            and critiquing small details, as well as optimizing applications.
+            Working in a team setting is ideal, because more knowledge is
+            obtained and retained between developers. In the next phase of my
+            career, I want to continue to be challenged and tested as a means to
+            maintain growth as a developer. In my off time I enjoy going to
+            sporting and music events, drone piloting, fishing, cycling, and
+            weight lifting. My past times, and my canine companions Belle and
+            Scout keep me active between going on walks and frequenting the dog
+            park. They even try their paws in development by nudging me as a
+            reminder to take a step back and clear my head to remain effective.
           </p>
           <p className="card-text text-white font-weight-bold">
             React | Express | Node | MongoDB | SQL | JavaScript | jQuery |
