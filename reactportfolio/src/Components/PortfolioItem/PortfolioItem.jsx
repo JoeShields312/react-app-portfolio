001ea9ee6902ca926c53e12397d7a5e33eccded8
Joe Shields <shields.john.joseph@gmail.com>
2020-05-06 21:22:23 -0500

    added summary to be returned

--- reactportfolio/src/Components/PortfolioItem/PortfolioItem.jsx
+++ reactportfolio/src/Components/PortfolioItem/PortfolioItem.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function PortfolioItem ({id, imageSrc, text, url}) {
+function PortfolioItem ({id, imageSrc, summary, text, url}) {
         return (
             <div key={id} id={id} className="portCard col-md-5 mb-4 mx-auto rounded">
                 <a href={url} target="__blank"><img src={imageSrc} alt={id} className="portImages"></img></a>
-                 <h3 className="projectText text-white">{text}</h3>
+                <p className="summaryText text-white center">{summary}</p>
+                <h3 className="projectText text-white">{text}</h3>
             </div>
         )
     };
