65c0e29e33eaae58e82288dcd4c8bb346ee1945f
Joe Shields <shields.john.joseph@gmail.com>
2020-05-19 23:02:10 -0500

    portfolio box size changed

--- reactportfolio/src/Components/PortfolioItem/PortfolioItem.jsx
+++ reactportfolio/src/Components/PortfolioItem/PortfolioItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function PortfolioItem ({id, imageSrc, summary, text, url}) {
         return (
-            <div key={id} id={id} className="portCard col-md-7 mb-4 mx-auto rounded">
+            <div key={id} id={id} className="portCard col-md-5 mb-4 mx-auto rounded">
                 <a href={url} target="__blank"><img src={imageSrc} alt={id} className="portImages"></img></a>
                 <p className="summaryText text-white center">{summary}</p>
                 <h3 className="projectText text-white">{text}</h3>
