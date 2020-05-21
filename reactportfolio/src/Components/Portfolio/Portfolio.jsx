146ce2e72115632df8f2117e8e97a00d9df20a5f
Joe Shields <shields.john.joseph@gmail.com>
2020-05-20 23:55:13 -0500

    portfolio style changes

--- reactportfolio/src/Components/Portfolio/Portfolio.jsx
+++ reactportfolio/src/Components/Portfolio/Portfolio.jsx
@@ -19,49 +19,73 @@ import PortfolioItem from '../PortfolioItem/PortfolioItem';
         imageSrc: "http://drive.google.com/uc?export=view&id=1Thjj6mUKPbY57_GimZ6bJwAcWv_QVMqw",
         url: "https://classroom-consilience.herokuapp.com/",
         summary: "Educational platform designed to allow teachers and students easy access to their curriculum.",
-        text: "Consilience"
+        text: "Consilience",
+        iconG: "http://drive.google.com/uc?export=view&id=1lo1f9F9_SCdCkPxrKd0o39DebWbpk-jx",
+        iconH: "http://drive.google.com/uc?export=view&id=1J_PQ7St_8EEAAqozax2U3QIRbbT38w85",
+        urlG: "https://github.com/JShields30/Consilience"
       },
       { id: 2,
         imageSrc: "http://drive.google.com/uc?export=view&id=1BXVltJ1YBI5T4WkwcS-CUe4GouU62DxD",
         url: "https://budget-tracker25.herokuapp.com/",
         summary: "App that allows users to keep track of their spending and income, by transaction names and amounts. The program uses a line chart to give the user a visual understanding when it comes to their spending and income.",
-        text: "Budget Tracker"
+        text: "Budget Tracker",
+        iconG: "http://drive.google.com/uc?export=view&id=1lo1f9F9_SCdCkPxrKd0o39DebWbpk-jx",
+        iconH: "http://drive.google.com/uc?export=view&id=1J_PQ7St_8EEAAqozax2U3QIRbbT38w85",
+        urlG: "https://github.com/JShields30/BudgetTrackers"
       },
       { id: 3,
         imageSrc: "http://drive.google.com/uc?export=view&id=1W7t-3fILUuh9u3gN5ToE1gM61jdVtJcR",
         url: "https://glacial-plateau-02354.herokuapp.com/",
         summary: "App that allows users the ability to search through a directory of employees to find specifically who they are searching for in the company. Search criteria for this app is only for by first and last name. App can be implemented with any companies directory and customized to search by department, name, or email. ",
-        text: "Employee Directory"
+        text: "Employee Directory",
+        iconG: "http://drive.google.com/uc?export=view&id=1lo1f9F9_SCdCkPxrKd0o39DebWbpk-jx",
+        iconH: "http://drive.google.com/uc?export=view&id=1J_PQ7St_8EEAAqozax2U3QIRbbT38w85",
+        urlG: "https://github.com/JShields30/BudgetTrackers"
       },
       { id: 4,
         imageSrc: "http://drive.google.com/uc?export=view&id=1mpJRCsHUOzgui5izM2tFAR3lVTSPnATr",
         url: "https://jshields30.github.io/GroupProject1/",
         summary: "App that allows users the ability to find the top ten films in theaters, choose a film, and have access to movie details of the chosen film. They then see the nearest 5 theaters, which includes the next showing of the film they chose. Once they select a cinema, directions to the cinema populates. New results update if they select different movies or cineams.",
-        text: "Movies & Groovies"
+        text: "Movies & Groovies",
+        iconG: "http://drive.google.com/uc?export=view&id=1lo1f9F9_SCdCkPxrKd0o39DebWbpk-jx",
+        iconH: "http://drive.google.com/uc?export=view&id=1J_PQ7St_8EEAAqozax2U3QIRbbT38w85",
+        urlG: "https://github.com/JShields30/GroupProject1"
       },
       { id: 5,
         imageSrc: "http://drive.google.com/uc?export=view&id=1CkQj9YZwkUa9rQvyNQxXexfpMv-Vceid",
         url: "https://nutridish1.herokuapp.com/",
         summary: "App that allows users to search for recipes that are high in a specific nutrient. User will then have the ability to save the recipe to their login.",
-        text: "NutriDish"
+        text: "NutriDish",
+        iconG: "http://drive.google.com/uc?export=view&id=1lo1f9F9_SCdCkPxrKd0o39DebWbpk-jx",
+        iconH: "http://drive.google.com/uc?export=view&id=1J_PQ7St_8EEAAqozax2U3QIRbbT38w85",
+        urlG: "https://github.com/JShields30/Project_2"
       },
       { id: 6,
         imageSrc: "http://drive.google.com/uc?export=view&id=1Kriecd39WMkbNfM_Vc-_NvFGX9GLgg7R",
         url: "https://jshields30.github.io/WeatherDashboard/",
         summary: "Weather applications that allows users to search the current Weather, 5 day forecast, also stores a list of cities searched. ",
-        text: "Weather Dashboard"
+        text: "Weather Dashboard",
+        iconG: "http://drive.google.com/uc?export=view&id=1lo1f9F9_SCdCkPxrKd0o39DebWbpk-jx",
+        iconH: "http://drive.google.com/uc?export=view&id=1J_PQ7St_8EEAAqozax2U3QIRbbT38w85",
+        urlG: "https://github.com/JShields30/WeatherDashboard"
       },
       { id: 7,
         imageSrc: "http://drive.google.com/uc?export=view&id=1L9eh8Ne7p-xLzjN9ICWYMYjBudqRrS1v",
         url: "https://jshields30.github.io/RandomPasswordGenerator/",
         summary: "App that generates a password with unique random charaters. Users selects the length of characters they want in their generated password",
-        text: "Random Password Generator"
+        text: "Random Password Generator",
+        iconG: "http://drive.google.com/uc?export=view&id=1lo1f9F9_SCdCkPxrKd0o39DebWbpk-jx",
+        iconH: "http://drive.google.com/uc?export=view&id=1J_PQ7St_8EEAAqozax2U3QIRbbT38w85",
+        urlG: "https://github.com/JShields30/RandomPasswordGenerator"
       },
       { id: 8,
         imageSrc: "http://drive.google.com/uc?export=view&id=1QU0aDcMbO0cac59w0VHRHcm1XX4cUC9z",
         url: "https://jshields30.github.io/AvengersQuiz/",
         summary: "Timed 10 question multiple choice quiz with 4 answer choices per question.",
-        text: "Avengers Quiz"
+        text: "Avengers Quiz",
+        iconG: "http://drive.google.com/uc?export=view&id=1lo1f9F9_SCdCkPxrKd0o39DebWbpk-jx",
+        iconH: "http://drive.google.com/uc?export=view&id=1J_PQ7St_8EEAAqozax2U3QIRbbT38w85",
+        urlG: "https://github.com/JShields30/AvengersQuiz"
       }
     ];
   //   )
@@ -77,7 +101,7 @@ function Portfolio() {
         return (
           <React.Fragment key={project.id}>
           <PortfolioItem
-          key={project.id} id={project.id} imageSrc={project.imageSrc} url={project.url} summary={project.summary} text={project.text} />
+          key={project.id} id={project.id} imageSrc={project.imageSrc} url={project.url} summary={project.summary} text={project.text} iconG={project.iconG} iconH={project.iconH} urlG={project.urlG}/>
 </React.Fragment>
         )
       })}
--- reactportfolio/src/Components/PortfolioItem/PortfolioItem.jsx
+++ reactportfolio/src/Components/PortfolioItem/PortfolioItem.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-function PortfolioItem ({id, imageSrc, summary, text, url}) {
+function PortfolioItem ({id, imageSrc, summary, text, url, urlG, urlH, iconH, iconG}) {
         return (
             <div key={id} id={id} className="portCard col-md-5 mb-2 mx-auto rounded">
                 <h3 className="projectText text-white">{text}</h3>
-                <a href={url} target="__blank"><img src={imageSrc} alt={id} className="portImages"></img></a>
+                <img src={imageSrc} alt={id} className="portImages"></img>
                 <p className="summaryText text-white center">{summary}</p>
+                <a href={urlG} target="__blank"><img src={iconG} alt={id} className="icon" data-toggle="tooltip" data-placement="top" title="GitHub Link"></img></a><a href={url} target="__blank"><img src={iconH} alt={id} className="icon" data-toggle="tooltip" data-placement="top" title="Heroku Link"></img></a>
             </div>
         )
     };
 
 export default PortfolioItem;
 
+{/* <a href="https://github.com/JShields30" target="__blank"className="github">
+    <i className="fab fa-github" data-toggle="tooltip" data-placement="top" 
+    title="GitHub Link"></i></a> */}
